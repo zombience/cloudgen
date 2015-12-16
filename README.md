0102858a3a7d726668a898ea5ff723bc0092598b
zombience <code@Iedlabs.net>
2015-12-15 23:08:56 -0600

    init and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# cloudgen
-This is a python port of a cloud/noise generator for use with the FadeCandy controller to drive LED outputs
+# Cloud Generator
+This is a python port of a cloud/noise generator for use with the FadeCandy controller to drive LED outputs. 
+I wanted to use the provided perlin noise example code as a mode in an installation, but couldn't run processing on a headless Raspberry Pi.
+Putting it in public in case anyone finds it useful. 
+
+## Requirements
+
+* This should work on Windows, Linux, OSX. I have used it on Win10, Win8, OSX, and Raspbian (Raspberry Pi)
+
+* the included opc.py file has been modified for use with Python3. If you wish to use python 2.7, replace the opc.py file with the  file provided in the fadecandy example code. Incidentally, if you use Touch Designer, this opc.py file can be used to communicate with fadecandy server in Touch Designer. 
+
+* You'll need to have the fadecandy server configured and running. There are several tutorials on how to get that started. I've linked one below. 
+
+## fadecandy info
+Thanks to Micah Scott for creating the FadeCandy controller and providing helpful examples!
+* https://github.com/scanlime/fadecandy
+
+## Tutorial
+
+* Got my project started with the following tutorial: https://learn.adafruit.com/1500-neopixel-led-curtain-with-raspberry-pi-fadecandy/overview
+
+## enjoy!
+
+
---
+++ cloudGenerator.py
@@ -0,0 +1,109 @@
+﻿#! /usr/bin/env python3
+
+#######################
+# CloudGenerator_standalone.py
+#
+# this is intended for when autonomous ambient shifting 
+# light is desired with no external input 
+#
+########################
+
+import opc
+import sys
+import time
+import math
+import perlin
+import json
+import os
+
+
+class CloudGenerator(object):
+
+    def __init__(self, verbose = True):
+        # definitions, settings
+
+        self.dx = 0
+        self.dy = 0
+        self.VERBOSE = verbose
+        settings = '' 
+        path = os.path.dirname(os.path.abspath(__file__))
+
+        with open(path + '/config.json') as config:
+            settings = json.load(config)
+        self.init_settings(settings)
+
+        # initialize OPC connecion
+        self.client = opc.Client('localhost:7890', True)
+        self.client._ensure_connected()
+
+    def init_settings(self, config):
+        if config == '':
+            config["speed"] = 0.01
+            config["size"]["width"] = 4
+            config["size"]["height"] = 64
+            config["noise_tile_size"] = 512
+
+        self.width = config["size"]["width"]
+        self.height = config["size"]["height"]
+        self.speed = config["speed"]
+        self.simplex = perlin.SimplexNoise(config["noise_tile_size"])
+        self.pixels = [(0,0,0)] * self.width * self.height	
+
+
+    def fractalNoise(self, x, y, z):
+        r = 0
+        amp = 1.0
+        for i in range(0, 4): 
+            r += self.simplex.noise3(x,y,z)
+            amp /= 2
+            x *= 2
+            y *= 2
+            z *= 2
+
+        return r
+
+    def log(self, msg):
+        if not self.VERBOSE:
+            return
+        print(msg)
+    
+    
+# main loop
+    def draw(self):
+        now = time.clock()
+        angle = math.sin(now * .001)
+        z = now * .00008
+        hue = now * .01
+        scale = .005
+
+        self.dx += math.cos(angle) * self.speed
+        self.dy += math.sin(angle) * self.speed
+
+        for x in range(self.width):
+            for y in range(self.height):
+                n = self.fractalNoise(self.dx + x * scale, self.dy + y * scale, z) - 0.75
+                m = self.fractalNoise(self.dx + x * scale, self.dy + y * scale, z + 10) - 0.75
+
+
+                color = (
+                    (hue + 80 * abs(m)) % 255,
+                    255 - 100 * max(min(math.pow(3.0 * abs(n), 3.5), 1.0), .6),
+                    255 * max(min(math.pow(3.0 * abs(n), 1.5), .3), .6)
+                    )
+
+                self.pixels[x + self.width * y] = color
+
+        self.client.put_pixels(self.pixels)
+
+
+
+clouds = CloudGenerator()
+
+
+# wait while socket is created
+time.sleep(2)
+
+
+while (True):
+	clouds.draw()
+	time.sleep(0.01)
